bd53fd737081d38f28e18040a307ef210c681ab2
arjun <arjun.maru@theonetechnologies.co.in>
2020-08-03 16:39:17 +0530

    Add owner feature module and lazy loading

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,7 +5,8 @@ import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
-  { path: 'home', component: HomeComponent}
+  { path: 'home', component: HomeComponent},
+  { path: 'owner', loadChildren: () => import('./owner/owner.module').then(m => m.OwnerModule) }
 ];
 
 @NgModule({
